24175123057f8512ae7bb46567be67a4097a5e6b
u0_a659 <124791706+u0-a659@users.noreply.github.com>
2025-12-13 17:13:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("fdroid").addEventListener("click", function() {
+	window.location.href = "https://f-droid.org/packages/com.termux/";
+});
+
+document.getElementById("github").addEventListener("click", function() {
+	window.location.href = "https://github.com/termux/";
+});
+
+document.getElementById("git").addEventListener("click", function() {
+	window.location.href = "https://github.com/termux/termux-x11";
+});
